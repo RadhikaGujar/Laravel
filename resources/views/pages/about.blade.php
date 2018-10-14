70695ce02a1590e851e76ea6dcd3f6bcce7b5c00
Radhika <rg583@njit.edu>
2018-10-14 15:49:20 -0400

    Task : Created pages home and about having actual content in pages directory

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+
+           <h1>About NJIT</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+
+           <h1>Welcome to NJIT HomePage</h1>
+@stop
